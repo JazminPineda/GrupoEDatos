3d71448d6668ca74c26e82003116a0c5785f090e
Jazmin <jppinedac940@hotmail.com>
2020-10-14 21:48:30 -0300

    se agrega modelo stock v1

---
+++ ModuloStock/Producto.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ModuloStock
+{
+    public class Producto
+    {
+        private int codigo;
+        private string nombre;
+        private double precio;
+        private Stack<string> elementos;
+        public Producto( int codigo, string nombre, double precio) 
+        {
+            this.codigo = codigo;
+            this.nombre = nombre;
+            this.precio = precio;
+            this.elementos = new Stack<string>(); // constructor
+
+        }
+
+        public void AgregarElemento(string nombre)
+        {
+            this.elementos.Push(nombre);
+
+        }
+        public string QuitarElemento()
+        {
+            return this.elementos.Pop();
+        }
+
+        public int MostrarCodigo()
+        {
+            return this.codigo;
+
+        }
+        public string MostrarNombre()
+        {
+            return this.nombre;
+        }
+        public double MostrarPrecio()
+        {
+            return this.precio;
+        }
+        
+        public int MostrarCantidad()
+        {
+            return this.elementos.Count;//Herenda la propiedad Count 
+        }
+
+    }
+}
